8d0a3e5046628c36e211e6dbc7c35725bc3688d7
Mohamed Ayoub Ghaddab <mohamedayoubghaddab@gmail.com>
2024-04-26 15:04:50 +0200

    first commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
-export const MONGO_URI =
-  "mongodb+srv://amirissaouiplan:XZGHbJYuGr7QPZcT@cluster0.aye1qwu.mongodb.net/micro_arch_foods";
+const MONGO_PASSWORD = "";
+
+export const MONGO_URI = `mongodb+srv://amirissaouiplan:${MONGO_PASSWORD}@cluster0.aye1qwu.mongodb.net/micro_arch_foods`;
 export const PORT = process.env.PORT || 8000;
-export const JWT_SECRET = "rupaulsdragracemarinaisqueennottia";
+export const JWT_SECRET = "";
